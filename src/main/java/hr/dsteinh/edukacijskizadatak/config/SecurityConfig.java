318e9208eec1c27111e423c20e2b345359efe527
Domagoj Steinhauzer <78277473+dsteinh@users.noreply.github.com>
2022-12-08 15:59:07 +0100

    add custom default user.

--- src/main/java/hr/dsteinh/edukacijskizadatak/config/SecurityConfig.java
+++ src/main/java/hr/dsteinh/edukacijskizadatak/config/SecurityConfig.java
@@ -2,8 +2,13 @@ package hr.dsteinh.edukacijskizadatak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +17,15 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers(HttpMethod.GET, "/api/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 }
